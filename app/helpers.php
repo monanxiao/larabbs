82302280db861514d153b6fb1ffb051c6354c0dd
monanxiao <monanxiao@qq.com>
2021-09-12 02:41:13 +0000

    分类话题列表

--- app/helpers.php
+++ app/helpers.php
@@ -5,3 +5,9 @@ function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+// 当前分类选中
+function category_nav_active($category_id)
+{
+    return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
--- routes/web.php
+++ routes/web.php
@@ -47,4 +47,8 @@ Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit'
 // Route::get('/users/{user}/edit', 'UsersController@edit')->name('users.edit');
 // Route::patch('/users/{user}', 'UsersController@update')->name('users.update');
 
+// 话题资源
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+// 分类资源
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
