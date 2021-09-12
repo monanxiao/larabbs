c620a05559ab0e021804d9e3d803b2e3082e5a6f
monanxiao <monanxiao@qq.com>
2021-09-12 03:17:30 +0000

    用户话题列表

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,10 @@ class User extends Authenticatable implements MustVerifyEmailContract
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    // 用户拥有的话题 一对多
+    public function topics()
+    {
+        return $this->hasMany(Topic::class);
+    }
 }
