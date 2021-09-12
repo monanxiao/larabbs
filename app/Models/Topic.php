c6490969334abf7dc88e9c699d1d3ab445b09a36
monanxiao <monanxiao@qq.com>
2021-09-12 02:13:52 +0000

    话题列表页面

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -11,4 +11,16 @@ class Topic extends Model
     protected $fillable = [
         'title', 'body', 'user_id', 'category_id', 'reply_count', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'
     ];
+
+    // 一个话题属于一个作者
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // 一个话题属于一个分类
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
