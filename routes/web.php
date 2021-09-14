6d932c1214c616e407be59f30ccd62cf7a3a5762
monanxiao <monanxiao@qq.com>
2021-09-14 10:53:25 +0000

    管理后台权限

--- routes/web.php
+++ routes/web.php
@@ -64,3 +64,7 @@ Route::resource('replies', 'RepliesController', ['only' => ['store', 'destroy']]
 
 // 通知列表
 Route::resource('notifications', 'NotificationsController', ['only' => ['index']]);
+
+// 无权限提醒
+Route::get('permission-denied', 'PagesController@permissionDenied')->name('permission-denied');
+
