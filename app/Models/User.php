de2d1b5d5e3103383589237506bd13010f88b31e
monanxiao <monanxiao@qq.com>
2021-09-15 13:53:11 +0000

    用户最后活跃时间

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,7 @@ use Spatie\Permission\Traits\HasRoles;
 class User extends Authenticatable implements MustVerifyEmailContract
 {
     // use HasApiTokens, HasFactory, Notifiable, MustVerifyEmailTrait;
+    use Traits\LastActivedAtHelper;
     use Traits\ActiveUserHelper;
     use HasRoles;
     use HasApiTokens, HasFactory, MustVerifyEmailTrait;
